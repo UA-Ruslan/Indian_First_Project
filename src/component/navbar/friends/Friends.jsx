33991f4c444da7cb11e213056c89a9a969b8f302
Ruslan Nikolaiets <mnemonikus69@gmail.com>
2023-01-09 11:33:40 +0200

    updating friends compnent

--- src/component/navbar/friends/Friends.jsx
+++ src/component/navbar/friends/Friends.jsx
@@ -3,38 +3,45 @@ import friendsModuleCss from "./Friends.module.css"
 import axios from "axios";
 import defaultAva from "../../../img/people/defaultAva.png"
 
-const Friends = (props) => {
-    if (props.users.length === 0) {
+class Friends extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get("http://localhost:4000/users").then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
-    return (
-        <div className={friendsModuleCss.friends}>
-            {props.users.map((user) => <div key={user.id} className={friendsModuleCss.userWrapper}>
-                <div className={friendsModuleCss.imgWrapper}>
-                    <img className={friendsModuleCss.userImg} src={user.imgUrl != null ? user.imgUrl : defaultAva} alt='Photo'/>
-                </div>
-                <div className={friendsModuleCss.userInfoWrapper}>
-                    <div>
-                        Nickname:{user.name}
+
+
+    render() {
+        return (
+            <div className={friendsModuleCss.friends}>
+                {this.props.users.map((user) => <div key={user.id} className={friendsModuleCss.userWrapper}>
+                    <div className={friendsModuleCss.imgWrapper}>
+                        <img className={friendsModuleCss.userImg} src={user.imgUrl != null ? user.imgUrl : defaultAva}
+                             alt='Photo'/>
                     </div>
-                    <div>
-                        From:{user.location}
+                    <div className={friendsModuleCss.userInfoWrapper}>
+                        <div>
+                            Nickname:{user.name}
+                        </div>
+                        <div>
+                            From:{user.location}
+                        </div>
                     </div>
-                </div>
-                <div className={friendsModuleCss.userBtnWrapper}>
-                    {user.following
-                        ? <button className={friendsModuleCss.userBtn} onClick={() => {
-                            props.unfollow(user.id)
-                        }}>unfollow</button>
-                        : <button className={friendsModuleCss.userBtn} onClick={() => {
-                            props.follow(user.id)
-                        }}>follow</button>
-                    }
-                </div>
-            </div>)}
-        </div>
-    )
+                    <div className={friendsModuleCss.userBtnWrapper}>
+                        {user.following
+                            ? <button className={friendsModuleCss.userBtn} onClick={() => {
+                                this.props.unfollow(user.id)
+                            }}>unfollow</button>
+                            : <button className={friendsModuleCss.userBtn} onClick={() => {
+                                this.props.follow(user.id)
+                            }}>follow</button>
+                        }
+                    </div>
+                </div>)}
+            </div>
+        )
+    }
 }
+
 export default Friends;
